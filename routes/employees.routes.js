07f3b7f5b9f1fd7284f0dca1a512efaebeb7fc37
ArtIPok <apokora@itsap.pl>
2021-06-04 11:04:43 +0200

    Add task 1

---
+++ routes/employees.routes.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const router = express.Router();
+const db = require('./../db');
+
+router.get('/employees', (req, res) => {
+  req.db.collection('employees').find().toArray((err, data) => {
+    if(err) res.status(500).json({ message: err });
+    else res.json(data);
+  })
+});
+
+router.get('/employees/random', (req, res) => {
+  req.db.collection('employees').aggregate([ { $sample: { size: 1 } } ]).toArray((err, data) => {
+    if(err) res.status(500).json({ message: err });
+    else res.json(data[0]);
+  })
+});
+
+router.get('/employees/:id', (req, res) => {
+  req.db.collection('departments').findOne({ _id: ObjectId(req.params.id) }, (err, data) => {
+    if(err) res.status(500).json({ message: err });
+    else if(!data) res.status(404).json({ message: 'not found' });
+    else res.json(data);
+  })
+});
+
+router.post('/employees', (req, res) => {
+  const { firstName, lastName } = req.body;
+  req.db.collection('departments').insertOne({ firstName: firstName, lastName: lastName }, err => {
+    if(err) res.status(500).json({ message: err });
+    else res.json({ message: 'OK' });
+  })
+});
+
+router.put('/employees/:id', (req, res) => {
+  const { firstName, lastName } = req.body;
+  req.db.collection('departments').updateOne({ _id: ObjectId(req.params.id) }, { $set: { firstName: firstName, lastName: lastName }}, err => {
+    if(err) res.status(500).json({ message: err });
+    else if(!data) res.status(404).json({ message: 'not found' });
+    else res.json({ message: 'OK' });
+  });
+});
+
+router.delete('/employees/:id', (req, res) => {
+  req.db.collection('departments').deleteOne({ _id: ObjectId(req.params.id) }, err => {
+    if(err) res.status(500).json({ message: err });
+    else if(!data) res.status(404).json({ message: 'not found' });
+    else res.json({ message: 'OK' });
+  });
+});
+
+module.exports = router;
